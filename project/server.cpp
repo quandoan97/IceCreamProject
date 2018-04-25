2069dd2961b77e3ddff96694a20ed0f5ebfe63e5
Quan <qdoan97@gmail.com>
2018-04-24 23:54:02 -0500

    X

---
+++ project/server.cpp
@@ -0,0 +1,13 @@
+#include "server.h"
+
+string Server::getName(){
+	return name;
+}
+
+double Server::getSalary(){
+	return salary;
+}
+
+double Server::getPaycheck(){
+	return paycheck;
+}
--- project/store.h
+++ project/store.h
@@ -8,32 +8,16 @@
 
 class Store{
 	private:
-		vector<Flavor> totalFlavors;
-		vector<Container> totalContainers;
-		vector<Toppings> totalToppings;
+		Inventory inventory;
+		vector <Server> servers;
+		Manager manager;
+		vector <Customer> customers;
+		double cash_register;		
 	public:
-		//Create a Flavor, Container, or Topping
-		void createFlavor();
-		void createTopping();
-		void createContainer();
-	
-		//Add the items to there respected vectors
-		void pushbackFlavors(Flavor& item);
-		void pushbackToppings(Toppings& item);
-		void pushbackContainers(Container& item);
-	
-		//These will print out all of the Flavors, Containers, and Toppings
-		void printFlavors();
-		void printContainers();
-		void printToppings();
-		void printAll();
-	
-		//These will return a specific flavor
-		Flavor returnFlavor(int index);
-		Toppings returnTopping(int index);
-		Container returnContainer(int index);
-	
-		//This willl create flavors, containers, and toppings
-		void easter_egg();
+		Store(){}
+		
+		void addtoInventory();
+		
+		
 };
 #endif
